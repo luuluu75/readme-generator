106d270ee005fe80f6c67c34cd5077c329769496
Thu Van <luuluu_1975@yahoo.com.au>
2021-02-23 18:06:20 +1100

    updated generate markdown file

--- develop/utils/generateMarkdown.js
+++ develop/utils/generateMarkdown.js
@@ -1,88 +1,51 @@
+function renderBadge(license){
+   return `![Github License](https://img.shields.io/badge/License-${license}-blue.svg)`;
+}
+
+function renderLink(license){
+   return `\n* [License](#license)\n`;
+}
+
 function generateMarkdown(userResponses) {
 
-   // Generate Table of Contents conditionally based on userResponses
-   let contentMenu = `## Table of Contents`;
- 
-   if (userResponses.installation !== '') { contentMenu += `
-   * [Installation](#installation)` };
- 
-   if (userResponses.usage !== '') { contentMenu += `
-   * [Usage](#installation)` };
- 
-   if (userResponses.contributing !== '') { contentMenu += `
-   * [Contributing](#contribute)` };
- 
- 
- 
-   // Generate markdown for the top required portions of the README
-   let draftMarkdown = 
-   `# ${userResponses.title}
-   ![Badge for GitHub repo top language](https://img.shields.io/github/languages/top/${userResponses.username}/${userResponses.repo}?style=flat&logo=appveyor) ![Badge for GitHub last commit](https://img.shields.io/github/last-commit/${userResponses.username}/${userResponses.repo}?style=flat&logo=appveyor)
+   return `${userResponses.title}
+   ${renderBadge(userResponses.license)}
+   ${renderLink(userResponses.license)}
+
+
+   ## CONTENTS
+
+   * [Description](#description)
    
-   Check out the badges hosted by [shields.io](https://shields.io/).
+   * [Installation](#installation)
+
+   * [Technologies](#technologies) 
+
    
-   
-   ## Description 
-   
+   ## DESCRIPTION 
+
    *The what, why, and how:* 
-   
+
    ${userResponses.description}
-   `
- 
-   // Add Table of Contents to markdown
-   draftMarkdown += contentMenu;
-  
-   // Add License section since License is required to Table of Contents
-   draftMarkdown += `
-   * [License](#license)`;
    
- 
-   // Optional Installation section
-   if (userResponses.installation !== '') {
-   
-   draftMarkdown +=
-   `
    
    ## Installation
    
    *Steps required to install project and how to get the development environment running:*
    
-   ${userResponses.installation}`
-   };
+   ${userResponses.installation}
+
    
+   ## Technologies
+   
+   *Steps required to install project and how to get the development environment running:*
+   
+   ${userResponses.technologies}
+
+   ## Dev Portfolio
+
+   * My GIT user name is ${userResponses.username}
+   * My reposistory is located ${userResponses.repo}`
+ };
  
-   // Optional Usage section
-   if (userResponses.usage !== '') {
-   
-   draftMarkdown +=
-   
-   `
-   
-   ## Usage
-   
-   ${userResponses.technology}`
-   };
-   
-   
-   // Optional Contributing section
-   if (userResponses.contribute !== '') {
-   `
-   
-   ## Contribute
-   
-   ${userResponses.contributing}`
-   };
-   
-   // License section is required
-   draftMarkdown +=
-   `
-   
-   ## License
-   
-   ${userResponses.license}
-   `;
- 
-   // Return markdown
-   return draftMarkdown; 
-  }
  module.exports = generateMarkdown;
